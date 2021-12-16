976208585dd389c0bfcb0d5c4212fa5d83da909d
DogGod777 <agraw17107@gapps.uwcsea.edu.sg>
2021-12-16 08:21:01 +0800

    Added mysql data and minor changes

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -5,10 +5,10 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <p>
+        <h1>
           Database test
-        </p>
-        <p id="test">Not loaded yet</p>
+        </h1>
+        <p id="test">Data Not Loaded</p>
         <button onClick={()=>{
           fetch("http://127.0.0.1:5000/api/read_db")
           .then(res => res.json())
@@ -17,6 +17,10 @@ function App() {
             text.innerHTML = "Welcome, " + data["name"];
           })
         }}>Read From Database</button>
+
+        <button onClick={()=>{
+          fetch("http://127.0.0.1:5000/api/write_db")
+        }}>Write To Database</button>
 
       </header>
     </div>
