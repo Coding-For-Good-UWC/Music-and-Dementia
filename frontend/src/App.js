f07fb17ad974bb99e6eca59217124430d6cd2f9d
Antoine-Lee <antoinekllee@gmail.com>
2022-02-19 14:02:06 +0800

    fixed master branch

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,41 @@
-import './App.css';
+import { Routes, Route } from 'react-router-dom'; 
 
-function App() {
-  return (
-    <div className="App">
-      
-    </div>
-  );
+import LandingPage from './pages/LandingPage'; 
+
+import LoginPage from './pages/LoginPage'; 
+import SignupPage from './pages/SignupPage'; 
+
+import CaregiverDashboard from './pages/CaregiverDashboard'; 
+
+import PatientPage from './pages/PatientPage'; 
+
+import PatientRegistration from './pages/PatientRegistration'; 
+
+function App() 
+{
+  return <div>
+    <Routes>
+      <Route path='/' element={ <LandingPage /> } />
+      <Route path='/login' element={ <LoginPage /> } />
+      <Route path='/signup' element={ <SignupPage /> } />
+      <Route path='/dashboard' element={ <CaregiverDashboard /> } />
+      <Route path='/patient' element={ <PatientPage /> } />
+      <Route path='/registerPatient' element={ <PatientRegistration /> } />
+    </Routes>
+  </div>; 
 }
 
 export default App;
+
+
+// import './App.css';
+
+// function App() {
+//   return (
+//     <div className="App">
+      
+//     </div>
+//   );
+// }
+
+// export default App;
